4dd3d4a181150dfee4764a85e41ff514ff867d92
Leslie <leslie.hinds82@gmail.com>
2018-12-03 16:07:28 -0700

    added unversioned javadocs

--- app/src/main/java/com/bluecirclesquare/projecthelper2/InvoiceFragment.java
+++ app/src/main/java/com/bluecirclesquare/projecthelper2/InvoiceFragment.java
@@ -1,4 +1,4 @@
-package com.bluecirclesquare.projecthelper2.view;
+package com.bluecirclesquare.projecthelper2;
 
 
 import android.app.Dialog;
@@ -12,34 +12,30 @@ import android.support.v7.app.AlertDialog.Builder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.bluecirclesquare.projecthelper2.model.entity.Invoice;
 
-import com.bluecirclesquare.projecthelper2.R;
-import com.bluecirclesquare.projecthelper2.model.entity.Quote;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class QuoteFragment extends DialogFragment {
-
+public class InvoiceFragment extends DialogFragment {
 
   @NonNull
   @Override
   public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
     AlertDialog.Builder builder = new Builder(getActivity());
     LayoutInflater inflater = LayoutInflater.from(getContext());
-    View view = inflater.inflate(R.layout.fragment_quote, null);
+    View view = inflater.inflate(R.layout.fragment_invoice, null);
     builder.setView(view);
     builder.setPositiveButton("OK", (dialog, id) -> {
-      Quote quote = new Quote();
-      quote.setAddress();
-      quote.setAmount();
-      quote.setContact();
-      quote.setDescription();
-      quote.setEvent();
-      quote.setQuoteNumber();
-      quote.setId();
+      Invoice invoice = new Invoice();
+      invoice.setAddress();
+      invoice.setPrice();
+      invoice.setDateCompleted();
+      invoice.setDateInvoiced();
+      invoice.setId();
       dialog.dismiss();
-      //TODO get all values in the dialog and set them in the quote
+      //TODO get all values in the dialog and set them in the job
 
     });
 
--- app/src/main/java/com/bluecirclesquare/projecthelper2/view/JobFragment.java
+++ app/src/main/java/com/bluecirclesquare/projecthelper2/view/JobFragment.java
@@ -1,49 +1,40 @@
 package com.bluecirclesquare.projecthelper2.view;
 
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AlertDialog.Builder;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-
 import com.bluecirclesquare.projecthelper2.R;
-import com.bluecirclesquare.projecthelper2.model.entity.Quote;
+import com.bluecirclesquare.projecthelper2.model.entity.Job;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class QuoteFragment extends DialogFragment {
-
+public class JobFragment extends DialogFragment {
 
   @NonNull
   @Override
   public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
     AlertDialog.Builder builder = new Builder(getActivity());
     LayoutInflater inflater = LayoutInflater.from(getContext());
-    View view = inflater.inflate(R.layout.fragment_quote, null);
+    View view = inflater.inflate(R.layout.fragment_job, null);
     builder.setView(view);
     builder.setPositiveButton("OK", (dialog, id) -> {
-      Quote quote = new Quote();
-      quote.setAddress();
-      quote.setAmount();
-      quote.setContact();
-      quote.setDescription();
-      quote.setEvent();
-      quote.setQuoteNumber();
-      quote.setId();
+      Job job = new Job();
+      job.setAddress();
+      job.setEvent();
+      job.setDescription();
+      job.setJobNumber();
+      job.setId();
       dialog.dismiss();
-      //TODO get all values in the dialog and set them in the quote
+      //TODO get all values in the dialog and set them in the job
 
     });
 
     builder.setNegativeButton("CANCEL", (dialog, id) -> {dialog.dismiss();});
     return builder.create();
   }
+
 }
